6730e7095eddfb06eb0f83e201b7477ce0e0dc0b
Javier Alejandro Oyarzo Carcamo <46333397+javiboin@users.noreply.github.com>
2025-08-30 19:57:43 -0300

    Add: Validaciones en los inputs de tipo text en Js

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-alert("Hola mundo");
+const submitFunction = (event) => {
+    event.preventDefault();
+    validarFormulario();
+}
+
+document.getElementById('formulario').addEventListener('submit', submitFunction);
+
+function validarFormulario() {
+    let camposTexto = document.querySelectorAll('input[type="text"]');
+    let validacionCorrecta = true;
+
+    camposTexto.forEach(campo => {
+        let errorCampo = document.getElementById('error' + campo.id.charAt(0).toUpperCase() + campo.id.slice(1));
+        if (campo.value.length == ''){
+            mostrarError(errorCampo, 'Este campo es obligatorio');
+            validacionCorrecta = false;
+        } else if (campo.value.length > 0 && campo.value.length < 3){
+            mostrarError(errorCampo, 'Este campo debe tener al menos 3 caracteres');
+            validacionCorrecta = false;
+        } else {
+            ocultarError(errorCampo);
+        }
+    });
+}
+
+const mostrarError = (elemento, mensaje) => {
+    elemento.textContent = mensaje;
+    elemento.style.display = "block";
+}
+
+const ocultarError = (elemento) => {
+    elemento.textContent = '';
+    elemento.style.display = 'none';
+}
